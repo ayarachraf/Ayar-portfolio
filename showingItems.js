dea4cdd1fb4aad24cd3a12f9abd3b02594e3b0ab
achraf ayar <107329106+ayarachraf@users.noreply.github.com>
2023-03-05 21:44:49 +0100

    anoter day another process

--- showingItems.js
+++ showingItems.js
@@ -1,11 +1,20 @@
 var backEndContent = document.getElementById("backendDiv");
 var frontEndContent = document.getElementById("fronendDiv");
+var officeContent = document.getElementById("officeDiv")
 
 function showBackEnd(){
     backEndContent.style.visibility = "visible";
-    frontEndContent.style.visibility = "hidden"
+    frontEndContent.style.visibility = "hidden";
+    officeContent.style.visibility = "hidden";
 }
 function showFrontEnd(){
     backEndContent.style.visibility = "hidden";
-    frontEndContent.style.visibility = "visible"
+    frontEndContent.style.visibility = "visible";
+    officeContent.style.visibility = "hidden";
+}
+function showOffice(){
+    backEndContent.style.visibility = "hidden";
+    frontEndContent.style.visibility = "hidden";
+    officeContent.style.visibility = "visible";
+
 }
